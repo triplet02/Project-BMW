85501f32f061fbc26293e7fc80b163260d4be2c0
triplet02 <triplet02@naver.com>
2022-10-08 23:14:42 +0900

    jumpForce adjusting

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,14 +10,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] GameObject map;
 
     // 게임 시작 위치 지정
-    Vector3 initialPlayerPosition = new Vector3(-5.0f, 0.0f, 0.0f);
-    Vector3 gameStartPlayerPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    Vector3 initialPlayerPosition = new Vector3(-5.0f, 0.01f, 0.0f);
+    Vector3 gameStartPlayerPosition = new Vector3(0.0f, 0.01f, 0.0f);
     Vector3 velocity = Vector3.zero;
     [SerializeField] float standByTime = 1.3f;
 
     // 점프 컨트롤
-    [SerializeField] float singleJumpForce = 5.0f;
-    [SerializeField] float doubleJumpForce = 6.0f;
+    [SerializeField] float jumpForce = 5.0f;
     [SerializeField] int maxJumpCount = 0;
     int jumpCount = 0;
     bool isDoubleJump = false;
@@ -35,6 +34,9 @@ public class PlayerController : MonoBehaviour
     CapsuleCollider capsuleCollider;
     [SerializeField] LayerMask layerMask = 0;
 
+    // Map 컨트롤
+    Rigidbody mapRigidbody;
+
     // 애니메이션
     Animator animator = null;
 
@@ -48,12 +50,15 @@ public class PlayerController : MonoBehaviour
         player.transform.position = initialPlayerPosition;
 
         rigidbody = player.GetComponent<Rigidbody>();
+        mapRigidbody = map.GetComponent<Rigidbody>();
         capsuleCollider = player.GetComponent<CapsuleCollider>();
         distance = capsuleCollider.bounds.extents.y + 0.05f;
         animator = player.GetComponentInChildren<Animator>();
 
         velocityMonitor = GameObject.Find("Velocity Monitor").GetComponent<Text>();
         debug = GameObject.Find("Game UI").GetComponent<Text>();
+
+        mapRigidbody.velocity = new Vector3(-3.0f, 0, 0);
     }
 
     // Update is called once per frame
@@ -62,8 +67,9 @@ public class PlayerController : MonoBehaviour
         // 게임 시작 위치로 캐릭터 위치 이동
         player.transform.position = Vector3.SmoothDamp(gameObject.transform.position, gameStartPlayerPosition, ref velocity, standByTime);
 
+        //맵 이동
         //map.transform.Translate(-0.006f, 0, 0);
-
+        
 
         CheckGround();
         if (isSlide)
@@ -76,12 +82,22 @@ public class PlayerController : MonoBehaviour
             "isDoubleJump : " + animator.GetBool("isDoubleJump").ToString() + "\n" +
             "isSlide : " + animator.GetBool("isSlide").ToString() + "\n" +
             "isGround : " + isGround.ToString() + "\n" +
-            "current anim : " + animator.GetCurrentAnimatorStateInfo(0).IsName("Run").ToString();
+            "y_axis_coord : " + player.transform.position.y.ToString();
     }
 
     public void TryJump()
     {
         //if (Input.GetKeyDown(KeyCode.Space)){}
+
+        //현재 있는 y축 좌표에 따라 점프 높이가 달라짐(??? 원인 모르겠음)
+        //y축 좌표로 점프할 때 부여하는 velocity에 보정해 주는 방식으로 일관성 있게 구현
+        float y_axis_coord = player.transform.position.y;
+        float adjustedJumpForce = jumpForce + y_axis_coord;
+
+        if(adjustedJumpForce < 0)
+        {
+            adjustedJumpForce = 1.0f;
+        }
 
         if (jumpCount < maxJumpCount)
         {
@@ -89,13 +105,13 @@ public class PlayerController : MonoBehaviour
             {
                 animator.SetBool("isJump", true);
                 isGround = false;
-                rigidbody.velocity = Vector3.up * singleJumpForce;
+                rigidbody.velocity = Vector3.up * adjustedJumpForce;
             }
             else
             {
                 animator.SetBool("isDoubleJump", true);
                 isDoubleJump = true;
-                rigidbody.velocity = Vector3.up * doubleJumpForce;
+                rigidbody.velocity = Vector3.up * adjustedJumpForce;
             }
             jumpCount++;
         }
