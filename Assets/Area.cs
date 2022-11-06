51da51807048e57b68d58af930c90502af95c04f
superchae <cocosuper76@gmail.com>
2022-11-06 17:47:24 +0900

    Add areaSpawner event

--- Assets/Area.cs
+++ Assets/Area.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Area : MonoBehaviour
 {
@@ -8,12 +9,14 @@ public class Area : MonoBehaviour
     private float destroyDistance = 45;
     [SerializeField]
     private float speed = 5;
+    public UnityEvent onAreaDestroyed;
     
     private void Update()
     {
         transform.position += Vector3.left * Time.deltaTime * speed;
         if(gameObject.transform.position.x < -30) {
             Destroy(gameObject);
+            onAreaDestroyed.Invoke();
         }
     }
 }
--- Assets/AreaSpawner.cs
+++ Assets/AreaSpawner.cs
@@ -7,21 +7,34 @@ public class AreaSpawner : MonoBehaviour
     [SerializeField]
     private GameObject[] areaPrefabs;
     private float xDistance = 15;
+    [System.NonSerialized]
+    private int currentIdx = 3;
 
-
-    private void Awake()
+    private void Start()
     {
-        SpawnArea();
+        InitializeArea();
     }
 
-    public void SpawnArea()
+//TODO: Need a branch - vertial / horizontal spawner
+    public void InitializeArea()
     {
         GameObject clone = null;
-        for (int i = 0; i < areaPrefabs.Length; i++)
+        for (int i = 0; i < 3; i++)
         {
             clone = Instantiate(areaPrefabs[i]);
-            clone.transform.position = new Vector3( i * xDistance, 0, 0);
+            clone.transform.position = new Vector3(i * xDistance, 0, 0);
         }
     }
-}
 
+    public void onAreaDestroyed()
+    {
+        Debug.Log("onAreadestroyed => " + currentIdx.ToString());
+        GameObject clone = null;
+        if (currentIdx < areaPrefabs.Length)
+        {
+            clone = Instantiate(areaPrefabs[currentIdx]);
+            clone.transform.position = new Vector3(xDistance, 0, 0);
+        }
+        currentIdx += 1;
+    }
+}
