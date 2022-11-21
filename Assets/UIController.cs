96481e08b546639059ba06106add7ad9fbce9395
triplet02 <triplet02@naver.com>
2022-11-21 17:50:26 +0900

    setting modal popup

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -5,6 +5,9 @@ using UnityEngine.EventSystems;
 
 public class UIController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] GameObject characterTooltip;
+    [SerializeField] GameObject settings;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +22,29 @@ public class UIController : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-
+        Debug.Log(eventData.pointerEnter.tag.ToString());
+        Debug.Log(eventData.pointerEnter.tag.Equals("SettingButton"));
+        if (!eventData.pointerEnter.tag.Equals("SettingButton"))
+        {
+            characterTooltip.SetActive(true);
+        } 
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if (!eventData.pointerEnter.tag.Equals("SettingButton"))
+        {
+            characterTooltip.SetActive(false);
+        }
+    }
 
+    public void ShowSettings()
+    {
+        settings.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settings.SetActive(false);
     }
 }
