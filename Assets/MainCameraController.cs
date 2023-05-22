6ca132d3eb94f83e2f9455459f77b7bb68a4287a
triplet02 <triplet02@naver.com>
2023-05-22 19:56:59 +0900

    camera walk modified : when jumping

--- Assets/MainCameraController.cs
+++ Assets/MainCameraController.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class MainCameraController : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    [SerializeField] float jumpCameraHeight;
     Vector3 cameraDistance = new Vector3(3.1f, 1.7f, -5.0f);
     Vector3 gameStartPlayerPosition = new Vector3(0.0f, 0.0f, 0.0f);
 
     int jumpCount = 0;
     bool isGround = true;
-    bool isDoubleJump = false;
     float yVelocity = 0.0f;
+    Vector3 jumpCameraPosition;
 
     Vector3 vel;
 
@@ -26,19 +27,24 @@ public class MainCameraController : MonoBehaviour
     {
         jumpCount = player.GetComponent<PlayerController>().GetJumpCount();
         isGround = player.GetComponent<PlayerController>().GetIsGround();
-        isDoubleJump = player.GetComponent<PlayerController>().GetIsDoubleJump();
-
         yVelocity = player.GetComponent<PlayerController>().Get_y_Velocity();
 
+        float playerXPosition = player.transform.position.x;
+        float playerYPosition = player.transform.position.y;
+        float playerZPosition = player.transform.position.z;
+
+        jumpCameraPosition = new Vector3(playerXPosition, playerYPosition < jumpCameraHeight ? playerYPosition : jumpCameraHeight, playerZPosition);
 
         if (jumpCount > 0)
         {
-            transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + cameraDistance, ref vel, 0.1f);
+            transform.position = Vector3.SmoothDamp(transform.position, jumpCameraPosition + cameraDistance, ref vel, 0.1f);
+            
+            //transform.position = Vector3.SmoothDamp(transform.position, jumpCameraPosition + cameraDistance, ref vel, 0.1f);
         }
 
         if(yVelocity < -0.2f && !isGround)
         {
-            transform.position = player.transform.position + cameraDistance;
+            transform.position = jumpCameraPosition + cameraDistance;
         }
 
         if (isGround)
