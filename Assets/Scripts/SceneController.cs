34d7ffb94cd12e6b7c5b55ead0acbe1d580b59f5
triplet02 <triplet02@naver.com>
2023-12-25 20:17:33 +0900

    APK build version

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -191,4 +191,9 @@ public class SceneController : MonoBehaviour
             Screen.orientation = ScreenOrientation.LandscapeLeft;
         }
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
